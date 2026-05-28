350fb9dfc1b74b38e2ee43a0173b89f848d7726d
sanjana20047 <gummadisanjana2004@gmail.com>
2026-05-28 22:47:40 +0530

    Add files via upload

---
+++ sub64bit.s
@@ -0,0 +1,14 @@
+		;program to add 64bit numbers
+		;first 32 bit number in R0 and R1
+		;second 32 bit number in R2 and R3
+		;R4 and R5 stores result
+		AREA addition,CODE,READONLY
+		ENTRY
+		LDR R0,=0x22222222
+		LDR R1,=0x55555555
+		LDR R2,=0x11111111
+		LDR R3,=0x44444444
+		SUBS R4,R0,R2
+		SBC R5,R1,R3
+STOP    B   STOP
+		END
