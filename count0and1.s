dbe0e01aa08683ba0095f44ed6ec61def74648e9
sanjana20047 <gummadisanjana2004@gmail.com>
2026-05-28 22:47:00 +0530

    Add files via upload

---
+++ count0and1.s
@@ -0,0 +1,21 @@
+			;program to count number of zeroes and ones
+			;given number is loaded to register or not
+			;R3 stores number of ones
+			;R4 stores number of zeroes
+			AREA count,CODE,READONLY
+			ENTRY
+			LDR R5,=NUM
+			LDR R0,[R5]
+			MOV R1,#32
+LOOP 		MOVS R0,R0,LSR#1
+			ADDCS R3,R3,#1
+			ADDCC R4,R4,#1
+			SUB R1,R1,#1
+			CMP R1,#0
+			BNE LOOP
+			NOP
+STOP        B  STOP
+			;beginning of data area
+			AREA SRC,DATA,READWRITE
+NUM         DCD  0x00000025
+			END
